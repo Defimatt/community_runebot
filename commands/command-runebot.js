18857f5ae9662dadcc5a5c7e935feb687df17d5f
Matt <8353780+Defimatt@users.noreply.github.com>
2021-10-20 17:54:36 +0100

    Create command-runebot.js

---
+++ commands/command-runebot.js
@@ -0,0 +1,8 @@
+const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
+
+await lib.discord.commands['@0.0.0'].create({
+  "guild_id": "857533189948964874",
+  "name": "runebot",
+  "description": "Find out how Runebot can help you",
+  "options": []
+});
